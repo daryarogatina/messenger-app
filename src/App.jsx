e7bfb1cb7efb54ba87fcfe7f14151720029c52db
Darya <rogatina.dv@gmail.com>
2023-04-19 23:20:41 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// это основной компонент нашего приложения; 
+// Он содержит логику для рендеринга маршрутов и получения данных с сервера.
+
+import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import {LoginPage} from "./pages/LoginPage";
+import {SignUpPage} from './pages/SignUpPage'
+import {RegFormPage} from './pages/RegFormPage';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LoginPage/>} />
+        <Route path="/signup" element={<SignUpPage/>} />
+        <Route path="/registration" element={<RegFormPage/>} />
+      </Routes>
+   </>
+  );
+}
+
+export default App; 
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,14 @@
+// Страница для регистрации логина и пароля пользователя
+
+// import React from 'react';
+// import SignUpContainer from './containers/SignUpContainer';
+
+const SignUpPage = () => {
+  return (
+  <div>
+  <h1> Sign up page </h1>
+  {/* <SignUpContainer /> */}
+  </div>
+);
+}
+export {SignUpPage};
